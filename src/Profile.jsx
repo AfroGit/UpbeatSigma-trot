660673ec9719ff9a803e8f6e02227648fb1bf714
AfroGit <17697323-AfroGit@users.noreply.replit.com>
2023-07-17 20:02:59 +0000

    This component serves the Headline of the site

---
+++ src/Profile.jsx
@@ -0,0 +1,10 @@
+export default function Profile(){
+    return(
+        <>
+        <article className='Banner'>
+        <h1>Anew Balance Counseling Services, PLLC</h1>
+   
+        </article>
+     </>
+    )
+}
