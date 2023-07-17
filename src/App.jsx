d3fb4a0895fe1ed675c21bfba8b83ad820a7d665
AfroGit <17697323-AfroGit@users.noreply.replit.com>
2023-07-17 20:15:24 +0000

    Banner CSS reviewed and container defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 
 import Logo from './Logo'
+import Profile from './Profile'
 
 export default function App() {
   return (
     <div>
       <Logo />
+      <Profile />
     </div>
   )
 }
