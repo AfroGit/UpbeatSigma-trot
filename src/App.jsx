9e837f097cff55ccd1e29141b6448f5a24c9e3c3
AfroGit <17697323-AfroGit@users.noreply.replit.com>
2023-07-17 20:50:10 +0000

    The main verbiage of the site is housed in the screed component.Challenges currently involve the suitable font and adding a selfpay link that actually works.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 
 import Logo from './Logo'
 import Profile from './Profile'
+import Screed from './Screed'
 
 export default function App() {
   return (
     <div className='App'>
       <Logo />
       <Profile />
+      <Screed />
     </div>
   )
 }
---
+++ src/Screed.jsx
@@ -0,0 +1,32 @@
+export default function Screed(){
+    return(
+        <>
+        <section className='September'>
+        <h2>Accepting New Clients</h2>
+        <p id='Waxx'>
+            Servicing adolescents and adults expiriencing mental health issues
+            such as anxiety, depression & trauma.
+            My theoretical orientation is integrative. 
+            This means, while I work primarily from a person-centered prospective, 
+            I also borrow concepts and techniques from other counseling theories such as CBT, 
+            DBT and REBT as appropriate.<br/>
+            During our sessions, you can expect to be in a judgment free environment. 
+            My goal is to assist you in finding the answers that help gain insight and instill 
+            a sense of hope.
+        </p>
+        <h3>Insurance Accepted:</h3>
+        <ul className='Fist'>
+            <li>NC Medicaid</li>
+            <li>Aetna</li>
+            <li>Blue Cross Blue Shield</li>
+            <li>Cigna & Evernorth</li>
+            <li>Oscar Health</li>
+            <li>Oxford</li>
+            <li>UnitedHealthcare</li>
+            <li><a href=''>Self-Pay</a></li>
+            <li>Sliding Scale also available</li>
+        </ul>
+        </section>
+        </>
+    )
+};
