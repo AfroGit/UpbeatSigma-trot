a530ff582d5acad86961657bb8d744685bdfacf7
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-07-24 22:23:08 +0000

    Added icons on to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return(
     <>
     <section className='Guu'>
-        <p><small>Website powered by aitechsolutions.dev &copy; 2023</small></p>
+        <p><small>Website powered by aitechsolutions.dev&copy; 2023</small></p>
         <p><small>&reg;ANEW BALANCE COUNSELING SERVICES(PLLC)</small></p>
     </section>
     </>
